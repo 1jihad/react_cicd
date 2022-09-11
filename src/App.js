f36156e1e4e8ab63aaa4a5c462b430fec6853665
1jihad <01jihadhassan@gmail.com>
2022-09-11 19:38:22 +0600

    change link text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Jihad Hassan
         </a>
       </header>
     </div>
