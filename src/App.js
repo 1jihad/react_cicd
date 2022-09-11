bce67199239a8b16901a97b7c420be9cf48accf7
1jihad <01jihadhassan@gmail.com>
2022-09-11 20:53:11 +0600

    delete git workflows files

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Jihad Hassan is a Good Programmer.
         </p>
         <a
           className="App-link"
