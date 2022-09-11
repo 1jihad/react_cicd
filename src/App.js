9fc25af647fc3c09fa751625f5d6fdbb6cde206d
1jihad <01jihadhassan@gmail.com>
2022-09-11 20:59:00 +0600

    add love in the anchor tag

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Jihad Hassan
+          Jihad hassan ❤️❤️❤️❤️
         </a>
       </header>
     </div>
